2836769634b4c7bcf3dd3e737a5139044e60e000
happiness123 <yuliang_furture@126.com>
2014-12-29 20:04:06 +0800

    Update websocketApp.py

--- python/websocketApp.py
+++ python/websocketApp.py
@@ -6,9 +6,13 @@ import socket
 import json
 import md5
 import logging
+import logging.config
 import hashlib
 from websocket import WebSocketException,WebSocket
 
+LOG_FILE = 'tst.log'
+logging.config.fileConfig('logging.conf')
+logger = logging.getLogger('tst')
             
 class WebSocketApp(object):
     """
@@ -72,7 +76,7 @@ class WebSocketApp(object):
             self._run_with_no_err(self.on_error, e)
         finally:
             self.sock.close()
-            self._run_with_no_err(self.on_close)
+            self._run_with_no_err(self.on_close,'closing')
             self.sock = None
 
     def _run_with_no_err(self, callback, *args):
@@ -84,80 +88,92 @@ class WebSocketApp(object):
                     logger.error(e)
 
 
-    #business
-    def buildMySign(params,secretKey):
-        sign = ''
-        for key in sorted(params.keys()):
-            sign += key + '=' + str(params[key]) +'&'
-        return  hashlib.md5(sign+'secret_key='+secretKey).hexdigest().upper()
+#business
+def buildMySign(params,secretKey):
+    sign = ''
+    for key in sorted(params.keys()):
+        sign += key + '=' + str(params[key]) +'&'
+    return  hashlib.md5(sign+'secret_key='+secretKey).hexdigest().upper()
 
-    #现货下单
-    def spotTrade(channel,api_key,secretkey,symbol,tradeType,price,amount):
-        params={
-          'api_key':api_key,
-          'symbol':symbol,
-          'type':tradeType,
-          'price':price,
-          'amount':amount
-        }
-        sign = buildMySign(params,secretkey)
-        return "{'event':'addChannel','channel':'"+channel+"','parameters':{'api_key':'"+api_key+"',\
-          'sign':'"+sign+"','symbol':'"+symbol+"','type':'"+tradeType+"','price':'"+price+"','amount':'"+amount+"'}}"
+#现货下单
+def spotTrade(channel,api_key,secretkey,symbol,tradeType,price='',amount=''):
+    params={
+      'api_key':api_key,
+      'symbol':symbol,
+      'type':tradeType
+     }
+    if price:
+        params['price'] = price
+    if amount:
+        params['amount'] = amount
+    sign = buildMySign(params,secretkey)
+    finalStr =  "{'event':'addChannel','channel':'"+channel+"','parameters':{'api_key':'"+api_key+"',\
+                'sign':'"+sign+"','symbol':'"+symbol+"','type':'"+tradeType+"'"
+    if price:
+        finalStr += ",'price':'"+price+"'"
+    if amount:
+        finalStr += ",'amount':'"+amount+"'"
+    finalStr+="}}"
+    return finalStr
 
-    #现货取消订单
-    def spotCancelOrder(channel,api_key,secretkey,symbol,orderId):
-        params = {
-          'api_key':api_key,
-          'symbol':symbol,
-          'order_id':orderId
-        }
-        sign = buildMySign(params,secretkey)
-        return "{'event':'addChannel','channel':'"+channel+"','parameters':{'api_key':'"+api_key+"','sign':'"+sign+"','symbol':'"+symbol+"','order_id':'"+orderId+"'}}"
+#现货取消订单
+def spotCancelOrder(channel,api_key,secretkey,symbol,orderId):
+    params = {
+      'api_key':api_key,
+      'symbol':symbol,
+      'order_id':orderId
+    }
+    sign = buildMySign(params,secretkey)
+    return "{'event':'addChannel','channel':'"+channel+"','parameters':{'api_key':'"+api_key+"','sign':'"+sign+"','symbol':'"+symbol+"','order_id':'"+orderId+"'}}"
 
-    #注册现货实时交易数据
-    def realtrades(channel,api_key,secretkey):
-        params={'api_key':api_key}
-        sign=buildMySign(params,secretkey)
-        return "{'event':'addChannel','channel':'"+channel+"','parameters':{'api_key':'"+api_key+"','sign':'"+sign+"'}}"
+#注册现货实时交易数据
+def realtrades(channel,api_key,secretkey):
+   params={'api_key':api_key}
+   sign=buildMySign(params,secretkey)
+   return "{'event':'addChannel','channel':'"+channel+"','parameters':{'api_key':'"+api_key+"','sign':'"+sign+"'}}"
 
-    #期货下单交易
-    def futureTrade(api_key,secretkey,symbol,contractType,price,amount,tradeType,matchPrice,leverRate):
-        params = {
-          'api_key':api_key,
-          'symbol':symbol,
-          'contract_type':contractType,
-          'price':price,
-          'amount':amount,
-          'type':tradeType,
-          'match_price':matchPrice,
-          'lever_rate':leverRate
-        }
-        sign = buildMySign(params,secretkey)
-        return "{'event':'addChannel','channel':'ok_futuresusd_trade','parameters':{'api_key':'"+api_key+"',\
-                'sign':'"+sign+"','symbol':'"+symbol+"','contract_type':'"+contractType+"','price':'"+price+"',\
-                'amount':'"+amount+"','type':'"+tradeType+"','match_price':'"+matchPrice+"','lever_rate':'"+leverRate+"'}}"
+#期货下单交易
+def futureTrade(api_key,secretkey,symbol,contractType,price='',amount='',tradeType='',matchPrice='',leverRate=''):
+    params = {
+      'api_key':api_key,
+      'symbol':symbol,
+      'contract_type':contractType,
+      'amount':amount,
+      'type':tradeType,
+      'match_price':matchPrice,
+      'lever_rate':leverRate
+    }
+    if price:
+        params['price'] = price
+    sign = buildMySign(params,secretkey)
+    finalStr = "{'event':'addChannel','channel':'ok_futuresusd_trade','parameters':{'api_key':'"+api_key+"',\
+               'sign':'"+sign+"','symbol':'"+symbol+"','contract_type':'"+contractType+"'"
+    if price:
+        finalStr += ",'price':'"+price+"'"
+    finalStr += ",'amount':'"+amount+"','type':'"+tradeType+"','match_price':'"+matchPrice+"','lever_rate':'"+leverRate+"'}}"
+    return finalStr
 
-    #期货取消订单
-    def futureCancelOrder(api_key,secretkey,symbol,orderId,contractType):
-        params = {
-          'api_key':api_key,
-          'symbol':symbol,
-          'order_id':orderId,
-          'contract_type':contractType
-        }
-        sign = buildMySign(params,secretkey)
-        return "{'event':'addChannel','channel':'ok_futuresusd_cancel_order','parameters':{'api_key':'"+api_key+"',\
-                'sign':'"+sign+"','symbol':'"+symbol+"','contract_type':'"+contractType+"','order_id':'"+orderId+"'}}"
+#期货取消订单
+def futureCancelOrder(api_key,secretkey,symbol,orderId,contractType):
+    params = {
+      'api_key':api_key,
+      'symbol':symbol,
+      'order_id':orderId,
+      'contract_type':contractType
+    }
+    sign = buildMySign(params,secretkey)
+    return "{'event':'addChannel','channel':'ok_futuresusd_cancel_order','parameters':{'api_key':'"+api_key+"',\
+            'sign':'"+sign+"','symbol':'"+symbol+"','contract_type':'"+contractType+"','order_id':'"+orderId+"'}}"
 
-    #期货实时交易数据
-    def futureRealTrades(api_key,secretkey):
-        params = {'api_key':api_key}
-        sign = buildMySign(params,secretkey)
-        return "{'event':'addChannel','channel':'ok_usd_future_realtrades','parameters':{'api_key':'"+api_key+"','sign':'"+sign+"'}}"
+#期货实时交易数据
+def futureRealTrades(api_key,secretkey):
+    params = {'api_key':api_key}
+    sign = buildMySign(params,secretkey)
+    return "{'event':'addChannel','channel':'ok_usd_future_realtrades','parameters':{'api_key':'"+api_key+"','sign':'"+sign+"'}}"
 
 def on_open(self):
     #国际站比特币现货行情
-    self.send("{'event':'addChannel','channel':'ok_btcusd_ticker'}")
+    #self.send("{'event':'addChannel','channel':'ok_btcusd_ticker'}")
 
     #国际站比特币期货当周合约行情
     #self.send("{'event':'addChannel','channel':'ok_btcusd_future_ticker_this_week'}")
@@ -166,8 +182,8 @@ def on_open(self):
     #self.send("{'event':'addChannel','channel':'ok_ltcusd_future_depth_next_week'}")
 
     #现货下单
-    #spotTradeMsg = spotTrade('ok_spotusd_trade','XXXX','XXXXXXXXXXXXXXXX','ltc_usd','buy','0.05','0.1')
-    #self.send(spotTradeMsg)
+    spotTradeMsg = spotTrade('ok_spotusd_trade','XXX','XXX','ltc_usd','buy_market','1','')
+    self.send(spotTradeMsg)
 
     #现货注册实时交易
     #realtradesMsg = realtrades('ok_usd_realtrades','XXXX','XXXXXXXXXXXXXXXX')
@@ -178,25 +194,25 @@ def on_open(self):
     #self.send(spotCancelOrderMsg)
 
     #期货下单
-    #futureTradeMsg = futureTrade('XXXX','XXXXXXX','btc_usd','this_week','2','2','1','0','20')
-    #self.send(futureTradeMsg)
+    futureTradeMsg = futureTrade('XXX','XXX','btc_usd','this_week','','2','1','1','20')
+    self.send(futureTradeMsg)
 
     #期货取消订单
     #futureCancelOrderMsg = futureCancelOrder('XXXX','XXXXXXX','btc_usd','65464','this_week')
     #self.send(futureCancelOrderMsg)
 
-     #期货注册实时交易
-     #futureRealTradesMsg = futureRealTrades('XXXX','XXXXXXX')
-     #self.send(futureRealTradesMsg)
+    #期货注册实时交易
+    #futureRealTradesMsg = futureRealTrades('XXXX','XXXXXXX')
+    #self.send(futureRealTradesMsg)
 def on_message(self,evt):
     data = json.loads(evt)
     print data
 
 def on_error(self,evt):
-    print 'error :'+evt
+    print 'error :',evt
 
 def on_close(self,evt):
-        print 'DISCONNECT'
+    print 'DISCONNECT'
 
 if __name__ == "__main__":
     url = "wss://real.okcoin.com:10440/websocket/okcoinapi"      #国内站请换成wss://real.okcoin.cn:10440/websocket/okcoinapi               
